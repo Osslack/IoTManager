c2bab2873596659d3c0cf70982a66f2208f8470a
simon <simon@simonDesktop.de>
2016-05-19 09:58:11 +0200

    Final Install Script

---
+++ nonsudo_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~;
+git clone https://github.com/Osslack/IoTManager;
+cd IoTManager;
+npm install;
+npm start;
--- sudo_install.sh
+++ sudo_install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10;
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list;
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
+sudo dpkg -i google-chrome-stable_current_amd64.deb;
+sudo rm -rf google-chrome-stable_current_amd64.deb;
+ sudo apt-get install -f;
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get install -y nodejs git mongodb-org;
-sleep 10;
-cd ~;
-git clone https://github.com/Osslack/IoTManager;
-cd ~/IoTManager/ejs-neu/;
-npm install -g babel-cli;
-npm install;
-npm start;
+sudo apt-get install -y nodejs git mongodb;
+sudo npm install -g babel-cli;
+cd /;
+sudo mkdir -p /data/db/;
+sudo service mongodb start;
