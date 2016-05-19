a2402aaa573fdfd7a185a8fb49323feab4c6aaf4
simon <simon@simonDesktop.de>
2016-05-19 10:45:18 +0200

    Fixed package.json

--- sudo_install.sh
+++ sudo_install.sh
@@ -2,7 +2,7 @@
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
 sudo dpkg -i google-chrome-stable_current_amd64.deb;
 sudo rm -rf google-chrome-stable_current_amd64.deb;
- sudo apt-get install -f;
+sudo apt-get install -fy;
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs git mongodb;
 sudo npm install -g babel-cli;
